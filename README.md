1c567a42e343450c0f85b38668d2c5722a664e2f
梁泰铭 <liangtaiming@liangtaimingdeMac-Studio.local>
2023-01-12 00:45:25 +0800

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # java-code-learn
 Something example about learning Java
+
+**记录一些学习 JDK17 的示例代码。**
---
+++ java17/src/basic/HelloWorld.java
@@ -0,0 +1,11 @@
+package basic;
+
+/**
+ * @author liangtaiming
+ * @date 2023/01/12
+ **/
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
